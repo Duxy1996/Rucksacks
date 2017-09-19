71851407ca49c6b0d456ef85ad49f27c70e06f5e
duxy1996 <durancdr96@gmail.com>
2017-09-19 19:35:35 +0200

    Other algorithm

--- problem_solution_two.py
+++ problem_solution_two.py
@@ -1,3 +1,27 @@
+
+def best_combination(rucksack_size,list_of_books):
+    if len(list_of_books) == 1:
+        if rucksack_size >= list_of_books[0][0]:
+            return list_of_books[0][1]
+        else:
+            return 0
+    else:
+        list_of_results = []
+        for book in list_of_books:
+            pass_list = []
+            for booki in list_of_books:                
+                if booki != book:
+                    pass_list.append(booki) 
+            add = 0
+            diff = 0
+            if  rucksack_size >= book[0]:
+                add = book[1]
+                diff = book[0]        
+
+            list_of_results.append(add + best_combination((rucksack_size - diff),pass_list))        
+        return max(list_of_results)
+
+
 rucksack_size = 0
 rucksack_books = []
 books_count = 0
@@ -21,32 +45,8 @@ while (count < books_count):
 
 print("List of books: " + str(list_of_books))
 
-first = list_of_books[0]
+print(best_combination(rucksack_size,list_of_books))
 
-for book in list_of_books:
-    temperol_book = (book[0],book[1],book[1]/(rucksack_size - book[0] + 1))
-    list_of_books_in_order.append(temperol_book)
-
-print(str(kilogrms_of_books)+' kgms of books')
-print(str(amount_of_money_posible)+' money of books')
-
-list_of_books_in_order = sorted(list_of_books_in_order, key=lambda x: x[2])
-list_of_books_in_order = list_of_books_in_order[::-1]
-
-winned_money = 0
-wight = 0
-count = 0
-while ((wight < rucksack_size) & (count < len(list_of_books_in_order))):
-    if(wight + list_of_books_in_order[count][0] <= rucksack_size):
-        rucksack_books.append(list_of_books_in_order[count])
-        wight = wight + list_of_books_in_order[count][0]
-        winned_money = winned_money + list_of_books_in_order[count][1]
-    count = count + 1
-
-print('In the rucksack there are/is these/this books:')
-print(rucksack_books)
-print('You have won all this money:')
-print(winned_money)
 
 
 
